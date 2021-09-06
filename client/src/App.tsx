a5eb6fef75a29df0858af6db46b7d8a15b4e958d
gherbi <yamin.gherbi@display-interactive.com>
2021-09-07 01:03:21 +0200

    install redux-toolkit tailwindCss

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>HEADER MENU</div>
+        <Router>
+            <div>
+                <Switch>
+                    {/*<Route path="/add-dog">*/}
+                    {/*    <About />*/}
+                    {/*</Route>*/}
+                    {/*<Route path="/listing-breed">*/}
+                    {/*    <Users />*/}
+                    {/*</Route>*/}
+                    <Route path="/">
+                        <App />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
     </div>
   );
 }
