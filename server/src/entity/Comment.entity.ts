f3e55c413162386f02ed937668a0c7d9d7229eb7
gherbi <yamin.gherbi@display-interactive.com>
2021-09-11 20:53:54 +0200

    add date in breedentity & commententity

--- server/src/entity/Comment.entity.ts
+++ server/src/entity/Comment.entity.ts
@@ -7,6 +7,9 @@ export class Comment {
     id: number;
 
     @Column()
+    date: Date = new Date();
+
+    @Column()
     comment: string;
 
     @ManyToOne(() => DogBreed, (dogBreedEntity) => dogBreedEntity.comment)
