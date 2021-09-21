b1739435d1a70810cde7dfaea6ea9985894698ac
yamin <gherbi.yamin.y@gmail.com>
2021-09-21 17:04:12 +0200

    wip

--- server/src/controller/BreedController.ts
+++ server/src/controller/BreedController.ts
@@ -3,58 +3,63 @@ import {File} from "multer";
 import {getRepository} from "typeorm";
 import {DogBreed} from "../entity/DogBreed.entity";
 import {addBreedSchema} from "../joi/breed/addBreedSchema";
-import {error} from "util";
 
 interface IMulterRequest extends Request {
     file: File;
 }
 
 class BreedController {
-
     async add(request: IMulterRequest, response: Response) {
-        const {body} = request;
+        const {body, file} = request;
+        const {name, description} = body;
+
         const breedRepository = getRepository(DogBreed);
-        const doesExist = await breedRepository.findOne({name: body.name});
-        if (doesExist) {
-            return response.json({failed: "Breed is already exist !"});
-        }
         try {
             const result = await addBreedSchema.validateAsync({
-                name: body.name,
-                description: body.description,
-                image: request.file.filename,
+                name,
+                description,
+                image: file.filename,
             });
+
+            if (await breedRepository.findOne({name})) {
+                return response.status(400).json({failed: "Breed is already exist !"});
+            }
+
             const breedCreated = breedRepository.create(result);
-            breedRepository.save(breedCreated).then(() => {
-                response.status(201).json({message: "Breed created !"});
-            }).catch((error) => response.status(400).json({ error }));
+            await breedRepository.save(breedCreated);
+            return response.status(201).json({success: "Breed created !"});
+
         } catch (error) {
-            return response.json({error});
+            const {details} = error;
+            return response.status(500).json({details});
         }
     }
 
     async all(request: Request, response: Response) {
-        const breedRepository = await getRepository(DogBreed);
+        const breedRepository = getRepository(DogBreed);
         const breed = await breedRepository.find();
-        return response.json( {breed});
-    }
-
-    async one(request: Request, response: Response) {
-        const breed = await getRepository(DogBreed).findOne(request.params.id);
-        if (!breed) {
-            return response.json({error: "Breed is not found"});
-        }
         return response.json({breed});
     }
 
-    async delete(request: Request, response: Response) {
+    async one(request: Request, response: Response) {
+        const {id} = request.params;
         const breedRepository = getRepository(DogBreed);
-        const breed = await breedRepository.findOne(request.params.id);
+        const breed = await breedRepository.findOne(id);
         if (!breed) {
-            return response.json({failed: "Breed is not found"});
+            return response.json({error: "Breed is not found"});
+        }
+        return response.status(200).json({breed});
+    }
+
+    async delete(request: Request, response: Response) {
+        const {id} = request.params;
+        const breedRepository = getRepository(DogBreed);
+        const breed = await breedRepository.findOne(id);
+        if (!breed) {
+            return response.status(400).json({failed: "Breed is not found"});
         }
         await breedRepository.remove(breed);
-        return response.json({success: "Breed delete !"});
+        return response.status(200);
     }
 }
 
