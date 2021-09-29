2db9fc90d6a39187c9d090626cd9e0fbfac3afb2
yamin <gherbi.yamin.y@gmail.com>
2021-09-29 19:10:07 +0200

    wip

--- server/src/utils/uploadImage.ts
+++ server/src/utils/uploadImage.ts
@@ -12,7 +12,7 @@ export const storage = multer.diskStorage({
 });
 
 const filefilter = (req, file, cb) => {
-    if (file.mimetype === "image/png" || file.mimetype === "image/jpeg") {
+    if (file.mimetype === "image/png" || file.mimetype === "image/jpeg" || file.mimetype === "image/jpg") {
         cb(null, true);
     } else {
         cb(null, false);
